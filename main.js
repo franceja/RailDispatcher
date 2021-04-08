af23108f665793294c66258e362a9b2384b1465e
Francis Chen <43563339+franceja@users.noreply.github.com>
2021-04-08 21:52:34 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const { app, BrowserWindow, webContents } = require('electron')
+const { app, BrowserWindow, webContents , screen} = require('electron')
 const path = require('path')
 var mainWindow = 0;
 var winUpdateID = 0;
 var winSimID = 0;
+var winMapID= 0;
+var latestStation = '';
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -76,6 +78,30 @@ function createSimWindow(){
     return win.id;
 }
 
+function createSimulation(station){
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize
+  const win = new BrowserWindow({
+    width: width,
+    height: height,
+    resizable: true,
+    frame: false,
+    icon:'src/img/icon.png',
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true,
+      enableRemoteModule: false,
+      preload: path.join(__dirname, 'preload.js'),
+    }
+    })
+
+    win.loadURL('file://'+__dirname+'/src/stations/'+station+'.html')
+    win.on('closed', function () {
+      mainWindow = null
+    })
+    return win.id;
+}
+
+
 app.on('ready',createWindow)
 
 app.on('window-all-closed', function () {
@@ -98,6 +124,7 @@ ipc.on('aSynMessage', (event, args) => {
   switch (args) {
     case 'close':
       app.quit()
+      
       break;
     
     case 'update':
@@ -118,7 +145,37 @@ ipc.on('aSynMessage', (event, args) => {
     case 'closeSim':
       BrowserWindow.fromId(winSimID).close();
       break;
+    case 'bangkokStn':
+      var rand = Math.floor((Math.random() * 3)) + 1;
+      event.sender.send('asynReply','<img class="simpics" src="stations/img/bangkok'+rand+'.jpg" width="100%"> This station has been added to this game since April 2021');
+      latestStation = 'bangkok';
+      break;
+    case 'bangsueGrandStn':
+      var rand = Math.floor((Math.random() * 2)) + 1;
+      event.sender.send('asynReply','<img class="simpics" src="stations/img/bangsueGrand'+rand+'.jpg" width="100%"> This station has been added to this game since April 2021');
+      latestStation = 'bangsueGrand';
+      break;
+    case 'nakhonPathomStn':
+      var rand = Math.floor((Math.random() * 1)) + 1;
+      event.sender.send('asynReply','<img class="simpics" src="stations/img/nakhonPathom'+rand+'.jpg" width="100%"> This station has been added to this game since April 2021');
+        latestStation = 'nakhonPathom';
+        break;
 
+    case 'updateStatus':
+      var version = app.getVersion();
+      console.log(version);
+      event.sender.send('asynReply',version);
+      break;
+    case 'submitSim':
+      BrowserWindow.fromId(winSimID).close();
+      winMapID = createSimulation(latestStation);
+      break;
+    
+    case 'closeMap':
+      console.log(winMapID);
+      BrowserWindow.fromId(winMapID).close();
+      break;
+      
     default:
       break;
   }
--- renderer.js
+++ renderer.js
@@ -1,29 +1,65 @@
 const ipc = require('electron').ipcRenderer
-
+//MAIN PAGE ELEMENT
 const closeBtn = document.getElementById('close');
 const updateBtn = document.getElementById('update');
 const startSim = document.getElementById('simStart');
-
+//UPDATE LIST
 const updateCloseBtn = document.getElementById('updateClose');
-
+const updateStatusDiv = document.getElementById('updateStatus');
+//SIMULATION PAGE
 const submitSimBtn = document.getElementById('submitSim');
 const closeSimBtn = document.getElementById('closeSim');
+const bangkokStnBtn = document.getElementById('bangkokStn');
+const bangsueGrandStnBtn = document.getElementById('bangsueGrandStn');
+const nakhonPathomStnBtn = document.getElementById('nakhonPathomStn');
 
+const closeMapBtn = document.getElementById('closeMap');
 
-//let replyDiv = document.getElementById('reply');
+function httpGet(theUrl)
+{
+    if (window.XMLHttpRequest){
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function(){
+        if (xmlhttp.readyState==4 && xmlhttp.status==200){
+            return xmlhttp.responseText;
+        }
+    }
+
+    xmlhttp.open("GET", theUrl, false );
+    xmlhttp.send();    
+}
+
+if(updateStatusDiv!= null){
+    let replyDiv = document.querySelector('#updateStatus');
+    ipc.send('aSynMessage','updateStatus');
+    ipc.on('asynReply', (event, args) => {
+        var currentVersion = '1.0.0';
+        var replyMsg = '<table class="table table-sm table-borderless"><tr><td>Release Version</td> <td>'+currentVersion+'</td></tr> <tr><td>Program version</td><td>'+args+'</td></tr></table>';
+        replyDiv.innerHTML = replyMsg;
+    });
+}
+
 if(closeBtn != null || updateBtn != null || startSim != null){
+
     closeBtn.addEventListener('click', () => {
         ipc.send('aSynMessage','close')
     });
-
     updateBtn.addEventListener('click', () => {
         ipc.send('aSynMessage','update')
     });
-
     startSim.addEventListener('click', () => {
         ipc.send('aSynMessage','startSim')
     });
 
+}
+
+if(closeMapBtn!= null){
+    closeMapBtn.addEventListener('click', () => {
+        ipc.send('aSynMessage','closeMap')
+    });
 }
 
 if(updateCloseBtn != null){
@@ -32,17 +68,33 @@ if(updateCloseBtn != null){
     });
 }
 
-if(submitSimBtn != null || closeSimBtn != null){
+if(submitSimBtn != null || closeSimBtn != null || bangkokStn != null || bangsueGrandStnBtn!= null || nakhonPathomStnBtn != null){
+
+    bangkokStnBtn.addEventListener('click', () => {
+        ipc.send('aSynMessage','bangkokStn')
+        submitSimBtn.classList.remove('disabled');
+        submitSimBtn.removeAttribute('disabled');
+    });
+    bangsueGrandStnBtn.addEventListener('click', () => {
+        ipc.send('aSynMessage','bangsueGrandStn')
+        submitSimBtn.classList.remove('disabled');
+        submitSimBtn.removeAttribute('disabled');
+    });
+    nakhonPathomStnBtn.addEventListener('click', () => {
+        ipc.send('aSynMessage','nakhonPathomStn')
+        submitSimBtn.classList.remove('disabled');
+        submitSimBtn.removeAttribute('disabled');
+    });
     submitSimBtn.addEventListener('click', () => {
         ipc.send('aSynMessage','submitSim')
     });
-
     closeSimBtn.addEventListener('click', () => {
         ipc.send('aSynMessage','closeSim')
     });
 
-}
+    let replyDiv = document.querySelector('#dialog');
 
-// ipc.on('asynReply', (event, args) => {
-//   replyDiv.innerHTML = args;
-// });
+    ipc.on('asynReply', (event, args) => {
+        replyDiv.innerHTML = args;
+    });
+}
